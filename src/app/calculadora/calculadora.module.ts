3e42fe0bc2c2c92c7f992ff58643aa8566421c7d
SergioCNCruz <sergiocncruz@gmail.com>
2020-02-26 20:34:28 -0300

    Cria componente calculadora

---
+++ src/app/calculadora/calculadora.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Calculadora/calculadoraComponent } from './calculadora/calculadora/calculadora.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [Calculadora/calculadoraComponent]
+})
+export class CalculadoraModule { }
---
+++ src/app/calculadora/calculadora/calculadora/calculadora.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora/components',
+  templateUrl: './components/calculadora.component.html',
+  styleUrls: ['./components/calculadora.component.css']
+})
+export class CalculadoraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
