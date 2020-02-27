b1e4cdae4556fbe8d4650cf044ed78dca56a09a9
SergioCNCruz <sergiocncruz@gmail.com>
2020-02-27 01:17:03 -0300

    Cria service the calculadora

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -8,9 +8,34 @@ import { CalculadoraService } from '../services';
 })
 export class CalculadoraComponent implements OnInit {
 
+  private numero1: string;
+  private numero2: string;
+  private resultado: string;
+  private operacao: string;
+
   constructor(private calculadoraService: CalculadoraService ) { }
 
   ngOnInit() {
+    this.limpar()
+  }
+
+  limpar(): void {
+    this.numero1 = '0',
+    this.numero2 = null,
+    this.resultado = null,
+    this.operacao = null
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao == null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  concatenarNumero(numAtuarl, numConcat): string {
+    return numAtuarl + numConcat
   }
 
 }
